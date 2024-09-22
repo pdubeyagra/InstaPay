47e8f156acc6c4d2239d69e23e31ab3d597b3d91
pradyumn Dubey <pdubey.agra@gmail.com>
2024-09-22 14:06:49 +0530

    start project

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>SignIn page</div>
+  )
+}
+
+export default SignIn
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>SignUp page</div>
+  )
+}
+
+export default SignUp
